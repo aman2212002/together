abb3a5a6890b7e78bca0f502ef77bddab8f14794
aman singh <amanrajawat2212002@gmail.com>
2022-04-01 04:10:07 +0530

    bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import  Login from'./components/Login';
+import MainNote from './components/Notes/MainNote';
+import Calendar from './components/Timetable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+ 
+class App extends Component {
+  render() {
+    return (
+       <Router>
+           <div className="App">
+            
+            
+           <Routes>
+                 {/* <Route caseSensitive path='/' element={< Login />}></Route> */}
+                 <Route caseSensitive path='/login' element={< Home />}></Route>
+                 <Route caseSensitive path='/MainNote' element={< MainNote />}></Route>
+                 {/* <Route caseSensitive path='/Timetable' element={<Calendar/>}></Route> */}
+                 
+          </Routes>
+          </div>
+       </Router>
+   );
+
+  }
 }
-
+ 
 export default App;
