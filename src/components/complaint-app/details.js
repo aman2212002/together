ac7a4bbeececbbc173c10e62b8db7335ecd510c0
localhostd3veloper <gautamanand022@gmail.com>
2022-04-20 15:45:39 +0530

    react bootstrap setup

--- src/components/complaint-app/details.js
+++ src/components/complaint-app/details.js
@@ -1,57 +1,64 @@
-import { useState } from "react";
-
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 function Details() {
-    const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({});
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
-    }
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(inputs);
-        console.log("sucessfully submitted");
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    console.log("sucessfully submitted");
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-
-            <div>
-                <label>Enter your Registration number :
-                    <input
-                        type="Void"
-                        name="registrationno"
-                        value={inputs.registrationno || ""}
-                        onChange={handleChange}
-                    />
-                </label>
-                
-            </div>
-            <div>
-                <label>Enter your room number
-                <input
-                    type="number"
-                    name="roomno"
-                    value={inputs.roomno || ""}
-                    onChange={handleChange}
-                />
-            </label>
-            </div>
-            <div>
-                <label>Enter the problem<br/>
-                    <input
-                        type="text"
-                        name="problem"
-                        value={inputs.problem || ""}
-                        onChange={handleChange}
-                    />
-                </label>
-            </div>
-
-            <input type="submit" />
-        </form>
-    )
+  return (
+    <div className="complaint-box-details">
+      <Form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Registration No.
+            <Form.Control
+              type="Void"
+              name="registrationno"
+              value={inputs.registrationno || ""}
+              onChange={handleChange}
+              className=""
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Room No.
+            <Form.Control
+              type="number"
+              name="roomno"
+              value={inputs.roomno || ""}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Tell us the problem in breif
+            <br />
+            <Form.Control
+              as="textarea"
+              type="text"
+              name="problem"
+              value={inputs.problem || ""}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <Button type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 export default Details;
